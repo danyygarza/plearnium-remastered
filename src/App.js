c637b352fd3d92fc107a0418efb8aafd0e7b8d03
danygzatec <A00829404@tec.mx>
2023-08-30 12:22:54 -0600

    navbar

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+import master_terny from '../assets/master_terny.png'
+import ternium from '../assets//ternium.png'
+
+export default {
+    master_terny,
+    ternium
+}
